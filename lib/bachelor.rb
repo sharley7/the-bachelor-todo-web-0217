27e3bb021abbf8af60edd5236cd2089615e79f64
S. Marie Kav <siobhan.harley@gmail.com>
2017-02-04 20:26:20 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,74 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  first_name = nil
+    data.map do | season_num, contestants|
+      if season_num == season
+      contestants.map do | profile |
+        profile.map do |att, stat|
+          if stat == "Winner"
+            name_arr = profile["name"].split()
+            first_name = name_arr.first
+          end
+        end
+      end
+    end
+  end
+  return first_name
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestant_name = nil
+    data.map do | season_num, contestants|
+      contestants.map do | profile |
+        profile.map do |att, stat|
+          if stat.downcase == occupation.downcase
+            contestant_name = profile["name"]
+          end
+        end
+      end
+    end
+  return contestant_name
 end
+
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+from_town = 0
+  data.each do | season_num, contestants|
+    contestants.each do | profile |
+      profile.each do |att, stat|
+        if stat.downcase == hometown.downcase
+          from_town += 1
+        end
+      end
+    end
+  end
+  return from_town
 end
 
+
 def get_occupation(data, hometown)
-  # code here
+  data.find do | season_num, contestants|
+    contestants.find do | profile |
+      profile.find do |att, stat|
+        if stat.downcase == hometown.downcase
+        return profile["occupation"]
+      end
+    end
+  end
+ end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_counter = 0
+  divide_counter = 0
+  data.map do | season_num, contestants |
+    if season_num == season
+    contestants.map do |profile|
+      profile.map do |att, stat|
+         age_counter += profile["age"].to_i
+         divide_counter += 1
+      end
+      end
+    end
+  end
+  return (age_counter/divide_counter.to_f).round
 end
